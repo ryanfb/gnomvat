3ae47d8480447c9c6caefd6d5bb3733f74435533
Ryan Baumann <ryan.baumann@gmail.com>
2022-01-25 14:09:28 -0500

    Stub in Plato 436a/b

--- update-progress.sh
+++ update-progress.sh
@@ -4,8 +4,9 @@ COMPLETED=`grep '^ \+[\[<A-Z]' gnomvat.md | wc -l | tr -d ' ' | tr -d "\n"`
 # subtract 2 to account for formatting in entry 41
 # subtract 5 to account for formatting in entry 320
 # subtract 7 to account for formatting in entry 321
+# subtract 1 to account for formatting in entry 436
 # subtract 1 to account for formatting in entry 515
-COMPLETED="$(($COMPLETED - (2 + 5 + 7 + 1)))"
+COMPLETED="$(($COMPLETED - (2 + 5 + 7 + 1 + 1)))"
 PERCENTAGE_FLOAT="$(( $COMPLETED / 577.0 * 100))"
 PERCENTAGE=`printf "%.0f\n" "$PERCENTAGE_FLOAT"`
 echo "{\n  \"schemaVersion\": 1,\n  \"label\": \"Translation Progress\",\n  \"message\": \"${COMPLETED} / 577 (${PERCENTAGE}%)\",\n  \"color\": \"yellow\"\n}" > progress.json
